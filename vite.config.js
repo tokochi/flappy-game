c48e2c5926e9c5cdf990cdb06ad6a8f60662ba34
tokochi <79018782+tokochi@users.noreply.github.com>
2023-02-27 13:05:36 +0100

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 export default {
   // Define `base` because this deploys to user.github.io/repo-name/
-  base: './flappy-game/',
+  base: '/flappy-game/',
   build: {
     // Do not inline images and assets to avoid the phaser error
     // "Local data URIs are not supported"
